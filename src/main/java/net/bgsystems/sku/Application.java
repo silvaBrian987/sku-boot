1540440b658236551ffee69fe1bcf8ba0626ef61
Silva <L0624462@bancogalicia.com.ar>
2018-10-10 11:39:27 -0300

    owo

--- src/main/java/net/bgsystems/sku/Application.java
+++ src/main/java/net/bgsystems/sku/Application.java
@@ -3,10 +3,18 @@ package net.bgsystems.sku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 }
